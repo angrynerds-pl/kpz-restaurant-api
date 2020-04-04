6a9fd2000fb42b34bb620a860170cab024e5efd0
jakubfaldasz <jakub.faldasz@icloud.com>
2020-04-04 17:47:40 +0200

    Change project structure

--- KPZ-Restaurant-REST_API/Startup.cs
+++ KPZ-Restaurant-REST_API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace KPZ_Restaurant_API
+namespace KPZ_Restaurant_REST_API
 {
     public class Startup
     {
