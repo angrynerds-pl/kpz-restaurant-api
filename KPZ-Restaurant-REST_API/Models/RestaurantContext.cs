fbf5fa95cf266b37ffc84d6329f62df7f247420d
jakubfaldasz <jakub.faldasz@icloud.com>
2020-04-06 22:54:41 +0200

    Add DbSets to DbContext

--- KPZ-Restaurant-REST_API/Models/RestaurantContext.cs
+++ KPZ-Restaurant-REST_API/Models/RestaurantContext.cs
@@ -14,6 +14,12 @@ namespace KPZ_Restaurant_REST_API.Models
         }
 
         public DbSet<User> Users { get; set; }
-
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<ProductInOrder> ProductsInOrders{ get; set; }
+        public DbSet<Reservation> Reservations{ get; set; }
+        public DbSet<Room> Rooms{ get; set; }
+        public DbSet<Table> Tables{ get; set; }
     }
 }
