38d90f12dda8d37496e3b7bf4a6e1fb307cc3b49
jakubfaldasz <jakub.faldasz@icloud.com>
2020-04-06 22:31:24 +0200

    Add entities: room and table

---
+++ KPZ-Restaurant-REST_API/Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace KPZ_Restaurant_REST_API.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+    }
+}
