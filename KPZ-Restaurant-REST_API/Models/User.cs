35b78ca8d73c5183e0357c9f92eeee749ef95495
jakubfaldasz <jakub.faldasz@icloud.com>
2020-04-06 19:15:44 +0200

    Create Users table, add migration

---
+++ KPZ-Restaurant-REST_API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KPZ_Restaurant_REST_API.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Rights { get; set; }
+    }
+}
