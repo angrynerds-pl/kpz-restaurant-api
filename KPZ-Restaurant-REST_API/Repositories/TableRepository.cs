96685f6922811c9399ebe24f0ee402d5d099dce8
jakubfaldasz <jakub.faldasz@icloud.com>
2020-04-07 11:22:15 +0200

    Add Users and Tables repository

---
+++ KPZ-Restaurant-REST_API/Repositories/TableRepository.cs
@@ -0,0 +1,18 @@
+﻿using KPZ_Restaurant_REST_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KPZ_Restaurant_REST_API.Repositories
+{
+    public class TableRepository: RestaurantGeneric<Table>, ITableRepository
+    {
+        RestaurantContext _context;
+
+        public TableRepository(RestaurantContext context): base(context) 
+        {
+            _context = context
+        }
+    }
+}
