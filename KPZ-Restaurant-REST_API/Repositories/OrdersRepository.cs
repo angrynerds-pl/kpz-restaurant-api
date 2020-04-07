bc8b0eeee9dad417f2977ca6fa93f002d3e178ad
jakubfaldasz <jakub.faldasz@icloud.com>
2020-04-07 11:34:22 +0200

    Add Categories and Orders repositories

---
+++ KPZ-Restaurant-REST_API/Repositories/OrdersRepository.cs
@@ -0,0 +1,18 @@
+﻿using KPZ_Restaurant_REST_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KPZ_Restaurant_REST_API.Repositories
+{
+    public class OrdersRepository : RestaurantGeneric<Order>, IOrdersRepository
+    {
+        RestaurantContext _context;
+
+        public OrdersRepository(RestaurantContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
